8c98bf5e0e8c05c093a8740d3a62135809865554
leMomo12 <m.nowotni@freenet.de>
2024-07-31 19:01:31 +0200

    Initial Commit

---
+++ PDFConverter/PDFConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFConverterApp.swift
+//  PDFConverter
+//
+//  Created by Maurice Nowotni on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct PDFConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
